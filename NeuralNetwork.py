5c07490b58c3d56418e7a29752ef0e05be4ca7cd
GitNomis <58851878+GitNomis@users.noreply.github.com>
2020-01-09 18:30:45 +0100

    summary(required)

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,13 +5,50 @@ Created on Thu Jan  9 00:22:16 2020
 @author: pille
 """
 import pandas as pd
+import numpy as np
+
 
 class NeuralNetwork:
-    def __init__(self,X=None,y=None):
-        self.X=X
-        self.y=y
+    ActivationFunctions={ "relu" : (lambda x: max(0,x)), "id":(lambda x:x) }
+    def __init__(self,X,y):
+        self.layers = []
+        self.X=np.array(X)
+        self.y=np.array(y)
         
+    def addLayer(self,u,a="relu"):
+        if not self.layers:
+            d = self.X.shape[1]
+        else:
+            d = self.layers[-1][0].shape[1]
+        layer = np.random.rand(d,u)
+        if not a in NeuralNetwork.ActivationFunctions.keys():
+            print(str(a)+" is not an implemented activation function. Defaulted to \"relu\"")
+            a=NeuralNetwork.ActivationFunctions.get("relu")  
+        self.layers.append((layer,NeuralNetwork.ActivationFunctions.get(a)))
+        return self.layers
+    
+    def finalLayer(self):
+        d = np.unique(self.y).size
+        self.layers.append((np.random.rand(self.layers[-1][0].shape[1],d),NeuralNetwork.ActivationFunctions.get("id")))
+        
+    def predict(self,X):
+        X = np.array(X)
+        X = output(X)
+        return np.where(X==np.amax(X))[0]    
+        
+    def output(self,x):
+        for l in self.layers:
+            x = x.dot(l[0])
+        return x    
+        
+    def fit(self):
+        for x,c in X,y:
+            for l in self.layers:
+                x = x.dot(l[0])
+                
+                
     def loadPulsar(self):
+        self.layers = []
         data = pd.read_excel('.\HTRU_2.xlsx',header=None)
         self.X = data.iloc[:,:8].values
         self.y = data.iloc[:,8:].values.ravel()
--- TestScript.py
+++ TestScript.py
@@ -4,14 +4,44 @@ Created on Fri Dec 13 15:12:23 2019
 
 @author: pille
 """
-
+import scipy.io
 import NeuralNetwork
+import pandas as pd
+import numpy as np
+from scipy import stats
    
-NN = NeuralNetwork.NeuralNetwork() 
-NN.showData()
+data = pd.read_excel('.\HTRU_2.xlsx',header=None)
+X = data.iloc[:,:8].values
+y = data.iloc[:,8:].values.ravel()
 
-NN.loadPulsar()
-NN.showData()
+X=scipy.stats.zscore(X)
+negX=[]
+posX=[]
+for (x,c) in zip(X,y):
+    if(c==0):
+        negX.append(x)
+    else:
+        posX.append(x)
+negX=np.array(negX) 
+posX=np.array(posX)   
 
-NN.hello()       
+balancedX=[]
+balancedy=[]
+for i in range(0,1500):
+    balancedX.append(posX[i])
+    balancedy.append(1)
+for i in range(0,1500):
+    balancedX.append(negX[i])
+    balancedy.append(0)     
+balancedX = np.array(balancedX)
+balancedy = np.array(balancedy)    
+
+#NN = NeuralNetwork.NeuralNetwork([[1],[2],[3]],[0,0,0]) 
+#NN.loadPulsar()
+#k=NN.addLayer(10,"relu")
+#NN.finalLayer()
+#test = X[0]
+#print(NN.predict(test))
+
+       
 
